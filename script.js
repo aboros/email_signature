d92926123b4c10843ffaec9ecbc4266effec3af3
Adam Boros <hunaboros@gmail.com>
2013-10-01 16:19:05 +0200

    Further experimenting.

--- script.js
+++ script.js
@@ -19,29 +19,29 @@
  	} );
 
  	clip.on( 'load', function(client) {
-  	// alert( "movie is loaded" );
+  		// alert( "movie is loaded" );
 	} );
 
  	clip.on( 'complete', function(client, args) {
-  this.style.display = 'none'; // "this" is the element that was clicked
-  alert("Copied text to clipboard: " + args.text );
-} );
-
- 	clip.on( 'mouseover', function(client) {
-  // alert("mouse over");
-} );
-
- 	clip.on( 'mouseout', function(client) {
-  // alert("mouse out");
-} );
-
- 	clip.on( 'mousedown', function(client) {
-
-  // alert("mouse down");
-} );
-
- 	clip.on( 'mouseup', function(client) {
-  // alert("mouse up");
-} );
+	  // this.style.display = 'none'; // "this" is the element that was clicked
+	  alert("Copied text to clipboard: " + args.html );
+	} );
+	
+	clip.on( 'mouseover', function(client) {
+	  // alert("mouse over");
+	} );
+	
+	clip.on( 'mouseout', function(client) {
+	  // alert("mouse out");
+	} );
+	
+	clip.on( 'mousedown', function(client) {
+	
+	  // alert("mouse down");
+	} );
+	
+	clip.on( 'mouseup', function(client) {
+	  // alert("mouse up");
+	} );
 
  });
